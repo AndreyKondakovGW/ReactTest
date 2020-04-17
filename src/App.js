21af01a97fb227db672f8ea8aecee5e37033f407
handlessdude <61969421+handlessdude@users.noreply.github.com>
2020-04-17 08:23:48 +0300

    cleared unused route/switch importing in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 
